5521b1b98cbaf8f7bfb11c67c742109006ec9720
Chris Han <55549961+XiaoqingHan@users.noreply.github.com>
2025-12-04 18:36:51 +0100

    Update backup_data2.py

--- backup_data2.py
+++ backup_data2.py
@@ -1,4 +1,6 @@
 import sys
+import os
+
 #usage: python3 $0 neural.list result_dir
 
 sn_dict = {}
@@ -25,3 +27,25 @@ for line in open(sys.argv[2]):
 
 rm_list.close()
 conserve_list.close()
+
+
+#####
+f = open("rm_result.list",'r')
+tar_list = open("tar.list",'w')
+rm_list = open("rm.list",'w')
+
+lines = f.readlines()
+for line in lines:
+    line = line.strip('\n')
+    backup_id = os.path.basename(line)
+    a = str(backup_id)+".tar.gz"
+    b = "tar -czf "+a+" "+line+"\n"
+    tar_list.write(b)
+    c = "rm -rf "+line+"\n"
+    rm_list.write(c)
+
+tar_list.close()
+rm_list.close()
+
+
+#####
